761dac1a2efaf1043d2b7a3f466e6bd942ddb3ba
JhinDenem <derek9522@gmail.com>
2022-06-08 04:35:51 +0200

    Update

--- js/darkmode.js
+++ js/darkmode.js
@@ -14,13 +14,13 @@ if(localStorage.getItem("theme") == null){
 let localData = localStorage.getItem("theme");
 
 if(localData == "light"){
-    icon.src = "/elso_minta_oldal/Images/moon.png";
-    logo.src = "/elso_minta_oldal/Images/mainlogoblue2.png"; 
-    footerl.src = "/elso_minta_oldal/Images/mainlogoblue3.png"; 
+    icon.src = "../elsomintaoldal/Images/moon.png";
+    logo.src = "../elsomintaoldal/Images/mainlogoblue2.png"; 
+    footerl.src = "../elsomintaoldal/Images/mainlogoblue3.png"; 
     document.body.classList.remove("dark-theme");
 }else if(localData == "dark"){
-    icon.src = "/elso_minta_oldal/Images/sun.png";
-    logo.src = "/elso_minta_oldal/Images/mainlogoblue.png"; 
+    icon.src = "../elsomintaoldal/Images/sun.png";
+    logo.src = "../elsomintaoldal/Images/mainlogoblue.png"; 
     footerl.src = "/Images/mainlogoblue3.png"; 
     document.body.classList.add("dark-theme");
 }
@@ -28,14 +28,14 @@ if(localData == "light"){
 icon.onclick = function(){
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")){
-        icon.src = "/elso_minta_oldal/Images/sun.png";
-        logo.src = "/elso_minta_oldal/Images/mainlogoblue.png"; 
-        footerl.src = "/elso_minta_oldal/Images/mainlogoblue3.png"; 
+        icon.src = "../elsomintaoldal/Images/sun.png";
+        logo.src = "../elsomintaoldal/Images/mainlogoblue.png"; 
+        footerl.src = "../elsomintaoldal/Images/mainlogoblue3.png"; 
         localStorage.setItem("theme" , "dark");
     }else{
-        icon.src = "/elso_minta_oldal/Images/moon.png";
-        logo.src = "/elso_minta_oldal/Images/mainlogoblue2.png"; 
-        footerl.src = "/elso_minta_oldal/Images/mainlogoblue3.png"; 
+        icon.src = "../elsomintaoldal/Images/moon.png";
+        logo.src = "../elsomintaoldal/Images/mainlogoblue2.png"; 
+        footerl.src = "../elsomintaoldal/Images/mainlogoblue3.png"; 
         localStorage.setItem("theme" , "light");
     }
 }
